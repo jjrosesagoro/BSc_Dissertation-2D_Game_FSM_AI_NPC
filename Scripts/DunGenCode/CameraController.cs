4b449df7f87bd0bb0dc8148dbd80bfd7913d77f9
JJ Roses-Agoro <60401297+jjrosesagoro@users.noreply.github.com>
2021-11-17 16:47:14 +0000

    Add files via upload

---
+++ Scripts/DunGenCode/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+
+{
+    public Transform Player;
+    public float TimeDamp = 0.4f;
+    private Vector3 PositionOfCamera;
+    private Vector3 velocity = Vector3.zero;
+
+    public static CameraController instance; //instance used to store object data
+    public Room CurrentRoom; //method to hold the data for which room the player is in
+    public float CameraMovementSpeed; //method to control the speed at which the camera moves
+
+    void awake ()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PositionOfCamera = new Vector3(Player.position.x, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, PositionOfCamera, ref velocity, TimeDamp);
+
+        PositionAfterUpdate(); //updating the position of the camera so the predefined method is called here
+    }
+
+    void PositionAfterUpdate()
+    {
+        if(CurrentRoom == null)
+        {
+            return; //If the room isn't updated then the position of the camera just needs to stay the same and so it returns null
+        }
+
+        Vector3 PositionOfTarget = FetchTargetCameraPosition();
+        transform.position = Vector3.MoveTowards(transform.position, PositionOfTarget, Time.deltaTime * CameraMovementSpeed);
+        //using the position of the player and the movement speed to decide the speed at which the camera moves
+
+    }
+
+        Vector3 FetchTargetCameraPosition()
+        {
+            if(CurrentRoom == null)
+            {
+                return Vector3.zero;
+            }
+
+            Vector3 PositionOfTarget = CurrentRoom.GetRoomCentre(); //finding the position of the target within the current room whilst finding the value of the vector for the centre
+            PositionOfTarget.z = transform.position.z;
+            
+            Debug.Log("Got centre");
+            return PositionOfTarget;
+        }
+
+        public bool IsSwtichingScene() //bool to check when the player is transitioning between scenes so the camera can be updated
+        {
+            return transform.position.Equals( FetchTargetCameraPosition()) == false;
+        }
+    }
+
---
+++ Scripts/Enemy Scripts/PatrolBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBehaviour : StateMachineBehaviour
+{
+    private int RandomLocation;
+    public float speed;
+    private LocationsPatrolled patrol;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        patrol = GameObject.FindGameObjectWithTag("LocationsPatrolled").GetComponent<LocationsPatrolled>(); /*creating a list of potential vectors to be 
+        patrolled for the enemy to identify and follow*/
+
+        RandomLocation = Random.Range(0, patrol.PointsOfPatrol.Length);
+        /*calling the PointsOfPatrol method within an external class which is holding the details regarding the configured waypoints and 
+        giving the range a value of 0 so that whilst the AI patrols from waypoint to waypoint there will be no preferred waypoint*/
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector2.Distance(animator.transform.position, patrol.PointsOfPatrol[RandomLocation].position) > 0.2f)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, patrol.PointsOfPatrol[RandomLocation].position, speed * Time.deltaTime);
+        }
+         //transforming the position of the AI based on the random locations of the waypoints, by storing the range in an extremely finite float
+        else
+        {
+            RandomLocation = Random.Range(0, patrol.PointsOfPatrol.Length);
+            /*calling the PointsOfPatrol method within an external class which is holding the details regarding the configured waypoints and 
+        giving the range a value of 0 so that whilst the AI patrols from waypoint to waypoint there will be no preferred waypoint*/
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) { //fetches the input from the spacebar keypress to use as a trigger
+            animator.SetBool("isPatrolling", false); //setting the pool parameter for the animator to false upon key press
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
